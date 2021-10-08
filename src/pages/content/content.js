4bc188664088a7d6e0c71ed7490ce0fe30fbdf0e
Shru-Singh <gshrutisingh@gmail.com>
2021-10-08 16:56:22 +0530

    initial commit

---
+++ src/pages/content/content.js
@@ -0,0 +1,157 @@
+import "./content.css";
+
+function Content() {
+  return (
+    <div className="Content">
+      <div className="contentHeader">
+        <h1>Where All boAthead Belong</h1>
+      </div>
+      <div className="searchWrapper">
+        <div className="search">
+          <input type="text" name="name" />
+          <button>
+            <h3>search</h3>
+          </button>
+        </div>
+      </div>
+      <div className="trending">
+        <div className="trendingWrapper">
+          <h1>Top Trending</h1>
+          <div className="topTrendingItems">
+            <h3>Dancer</h3>
+            <h3>Singer</h3>
+            <h3>Model</h3>
+            <h3>Photographer</h3>
+            <h3>Car racer</h3>
+          </div>
+          <div className="trendingCards">
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+            <div className="trendingCard">
+              <div className="trendingPhotoCard"></div>
+              <p>
+                Feeling fiesty like never before. Channel your inner Rebel with
+                the all new T-Rebel collection.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="videoSpace"></div>
+      <div className="videosdiv">
+        <div className="videosWrapper">
+          <div className="videosCard">
+            <div className="videos"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="videosCard">
+            <div className="videos"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="videosCard">
+            <div className="videos"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="videosCard">
+            <div className="videos"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="news">
+        <h1>In the News</h1>
+        <div className="newsWrapper">
+          <div className="newsCard">
+            <div className="newsImageCard"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="newsCard">
+            <div className="newsImageCard"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="newsCard">
+            <div className="newsImageCard"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+          <div className="newsCard">
+            <div className="newsImageCard"></div>
+            <p>
+              Feeling fiesty like never before. Channel your inner Rebel with
+              the all new T-Rebel collection.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
